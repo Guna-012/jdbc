bb8686d1fe452cf329e438aabc9e077b70f631ca
Guna-012 <140234667+Guna-012@users.noreply.github.com>
2023-07-22 12:44:45 +0530

    Add files via upload

---
+++ factorial/src/number/factoriaL.java
@@ -0,0 +1,14 @@
+package number;
+
+public class factoriaL {
+	public static void main(String[] args) {
+		int fact = 1, num = 5;
+
+		for (int i = 1; i <= num; i++) {
+			fact = fact * i;
+		}
+		System.out.println(fact);
+
+	}
+
+}
